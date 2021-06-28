99955e2edca8a634fcbc0002ba5b7535d4431bee
Cardoso-topdev <oscar@cardoso.devstar.com>
2021-06-28 17:36:48 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Index from './pages';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Index}/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Index = () => {
+    return ( 
+        <h1>Hola Mundo</h1>
+     );
+}
+ 
+export default Index;
