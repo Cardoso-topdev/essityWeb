e55825e6c3fa7c6c53db9dfcfbd3d5555322e76f
Cardoso1205 <oscar1w2e@gmail.com>
2021-07-09 21:40:49 -0500

    Finalizando seccion about, cambiando fuente por DM SANS

---
+++ src/components/section-about/about.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+//resources
+import video from "../../assets/img/video.jpg";
+
+const About = () => {
+  return (
+    <section className="about">
+      <div className="section-about container">
+        <div>
+          <img src={video} />
+        </div>
+        <div className="info-about">
+          <h2>¿Quiénes somos?</h2>
+          <p className="p1">Essity es una compañía líder<br></br> en el mundo en higiene y salud.</p>
+          <p className="p2">
+            Estamos comprometidos a mejorar el bienestar con nuestros productos
+            y servicios.
+          </p>
+          <Link to="#" className="btn btn-primary btn-about">
+            Ver más
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import React, { Fragment } from 'react';
 //components
 import Navbar from '../components/navbar/navbar';
 import Header from '../components/header/header';
+import About from '../components/section-about/about';
 
 const Index = () => {
     return ( 
         <Fragment>
         <Navbar />
         <Header />
+        <About />
         </Fragment>
      );
 }
