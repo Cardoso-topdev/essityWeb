974b51392a7430cb4b3965ad934c21e3d24a2c2c
Cardoso1205 <oscar1w2e@gmail.com>
2021-10-04 23:59:13 -0500

    cambios popup

--- src/components/asesoria-secciones/saba.js
+++ src/components/asesoria-secciones/saba.js
@@ -1,11 +1,7 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import PopSaba from "../popups/saba/popupSaba";
 
-import bn8 from "../../assets/img/saba/nocturnas/bn8.jpg";
-import bn10 from "../../assets/img/saba/nocturnas/bn10.jpg";
-import bn24 from "../../assets/img/saba/nocturnas/bn24.jpg";
-import con8 from "../../assets/img/saba/nocturnas/con8.jpg";
-
-import sabaNocturnas from '../../json/asesoria/saba/nocturnas.json';
+import sabaNocturnas from "../../json/asesoria/saba/nocturnas.json";
 
 import in10 from "../../assets/img/saba/delgadas/in10.jpg";
 import in42 from "../../assets/img/saba/delgadas/in42.jpg";
@@ -23,30 +19,34 @@ import regular16 from "../../assets/img/saba/diarios/regular16.jpg";
 import regular28 from "../../assets/img/saba/diarios/regular28.jpg";
 
 const SabaSeccion = () => {
+  const [trigger, setTrigger] = useState(false);
+  const [objeto, setObjeto] = useState({});
+
   return (
     <Fragment>
       <div className="productos-saba mt-5">
         <div className="container d-flex">
           <div className="div">
-          <div className="ml-5 pl-5 mb-5 saba-titulo">
-            <h3>Productos Saba</h3>
-            <p>Expande y fortalece tu portafolio</p>
+            <div className="ml-5 pl-5 mb-5 saba-titulo">
+              <h3>Productos Saba</h3>
+              <p>Expande y fortalece tu portafolio</p>
+            </div>
+            <p className="fs-30 fw-500 lh-42">
+              De cada 10 pesos de toalla femenina y pantiprotectores que se
+              consumen en el país, 6 son de Saba®
+            </p>
+            <p>
+              Saba se posiciona como líder en ambas categorías lo que ayudará a
+              incrementar las ventas de tu negocio; además, las toallas
+              sanitarias están consideradas como un derecho a la salud de las
+              mujeres como productos de primera necesidad por lo que siempre
+              buscamos innovar en nuestros productos. ¡Tus clientes siempre te
+              los pedirán!
+            </p>
           </div>
-          <p className="fs-30 fw-500 lh-42">
-            De cada 10 pesos de toalla femenina y pantiprotectores que se
-            consumen en el país, 6 son de Saba®
-          </p>
-          <p>
-            Saba se posiciona como líder en ambas categorías lo que ayudará a
-            incrementar las ventas de tu negocio; además, las toallas sanitarias
-            están consideradas como un derecho a la salud de las mujeres como
-            productos de primera necesidad por lo que siempre buscamos innovar
-            en nuestros productos. ¡Tus clientes siempre te los pedirán!
-          </p>
-        </div>
-        <div>
-          <img alt="imagen"/>
-        </div>
+          <div>
+            <img alt="imagen" />
+          </div>
         </div>
       </div>
 
@@ -55,34 +55,26 @@ const SabaSeccion = () => {
         <div className="card-productos mb-5">
           <h2 className="text-center pb-3">Nocturnas</h2>
           <div className="productos producto-flex">
-            <div className="producto">
-              <img src={bn8} alt="" />
-              <p className="text-center">
-                Buenas Noches
-                <br /> con alas 8 piezas
-              </p>
-            </div>
-            <div className="producto">
-              <img src={bn24} alt="" />
-              <p className="text-center">
-                Buenas Noches
-                <br /> con alas 24 piezas
-              </p>
-            </div>
-            <div className="producto">
-              <img src={bn10} alt="" />
-              <p className="text-center">
-                Buenas Noches
-                <br /> con alas 10 piezas
-              </p>
-            </div>
-            <div className="producto">
-              <img src={con8} alt="" />
-              <p className="text-center">
-                Confort Nocturna
-                <br /> con alas 8 piezas
-              </p>
-            </div>
+            {sabaNocturnas.map((item) => (
+              <Fragment>
+                <div
+                  className="producto"
+                  key={item.id}
+                  onClick={() => {
+                    setTrigger(true);
+                    setObjeto(item);
+                  }}
+                >
+                  <img src={item.image} alt="" />
+                  <p className="text-center">{item.info}</p>
+                </div>
+                <PopSaba
+                  trigger={trigger}
+                  setTrigger={setTrigger}
+                  item={objeto}
+                />
+              </Fragment>
+            ))}
           </div>
         </div>
 
