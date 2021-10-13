281ed4dff01faf39fa58d883ef9760fb507abf8e
Cardoso1205 <oscar1w2e@gmail.com>
2021-10-13 10:54:53 -0500

    navbar responsive

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,60 +1,90 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Logo from "../../assets/img/Logo.svg";
 import { GoThreeBars } from "react-icons/go";
 
 const Navbar = () => {
   return (
-    <div className="section-nav">
-      <div className="navigation container">
-        <button to="/" className="d-flex align-items-center">
-          <img src={Logo} alt="" />
-        </button>
-        <button type="button" className="btn-navbar">
-          <GoThreeBars />
-        </button>
-        <nav>
-          <div className="link" onClick={() => (window.location.href = "/")}>
-            Inicio
-          </div>
-          <div
-            className="link"
-            onClick={() => (window.location.href = "/quienes-somos")}
+    <Fragment>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src={Logo} />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-            ¿Quiénes somos?
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <div
+                  className="nav-link active"
+                  aria-current="page"
+                  onClick={() => (window.location = "/")}
+                >
+                  Inicio
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  className="nav-link active"
+                  onClick={() => (window.location = "/quienes-somos")}
+                >
+                  ¿Quiénes somos?
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  className="nav-link active"
+                  onClick={() => (window.location = "/promociones")}
+                >
+                  Promociones
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  className="nav-link active"
+                  onClick={() => (window.location = "/crece-mi-negocio")}
+                >
+                  Crece mi negocio
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  className="nav-link active"
+                  onClick={() => (window.location = "/asesoria")}
+                >
+                  Asesoría
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  className="nav-link active"
+                  onClick={() => (window.location = "/donde-comprar")}
+                >
+                  Donde comprar
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  className="nav-link active"
+                  onClick={() => (window.location = "/preguntas-frecuentes")}
+                >
+                  Preguntas frecuentes
+                </div>
+              </li>
+            </ul>
           </div>
-          <div
-            className="link"
-            onClick={() => (window.location.href = "/promociones")}
-          >
-            Promociones
-          </div>
-          <div
-            className="link"
-            onClick={() => (window.location.href = "/crece-mi-negocio")}
-          >
-            Crece mi negocio
-          </div>
-          <div
-            className="link"
-            onClick={() => (window.location.href = "/asesoria")}
-          >
-            Asesoría
-          </div>
-          <div
-            className="link"
-            onClick={() => (window.location.href = "/donde-comprar")}
-          >
-            Donde comprar
-          </div>
-          <div
-            className="link"
-            onClick={() => (window.location.href = "/preguntas-frecuentes")}
-          >
-            Preguntas frecuentes
-          </div>
-        </nav>
-      </div>
-    </div>
+        </div>
+      </nav>
+    </Fragment>
   );
 };
 
